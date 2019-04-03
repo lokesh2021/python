9aa9c78a32212a47ed49b59961f7a66951c315ae
lokesh2021 <lokeshbandi2021@gmail.com>
2019-04-03 22:08:26 +0530

    star using python turtle

---
+++ star.py
@@ -0,0 +1,11 @@
+Python 3.7.1rc2 (v3.7.1rc2:6c06ef7dc3, Oct 13 2018, 15:44:37) [MSC v.1914 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:\Users\acer\Desktop\polygon.py =================
+>>> 
+=== RESTART: C:\Users\acer\AppData\Local\Programs\Python\Python37\star.py ===
+>>> 
+=== RESTART: C:\Users\acer\AppData\Local\Programs\Python\Python37\star.py ===
+>>> 
+=== RESTART: C:\Users\acer\AppData\Local\Programs\Python\Python37\star.py ===
+>>> 
